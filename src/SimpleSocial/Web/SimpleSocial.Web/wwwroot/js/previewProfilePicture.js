5212f9daa72cce795f33be78186e7c040426fc66
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-04-17 02:55:14 +0300

    Added profile picture preview before uploading it finally.

---
+++ src/SimpleSocial/Web/SimpleSocial.Web/wwwroot/js/previewProfilePicture.js
@@ -0,0 +1,13 @@
+﻿function previewFile() {
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
