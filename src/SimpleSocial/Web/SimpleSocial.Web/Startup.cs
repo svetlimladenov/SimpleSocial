14f2d6a5e8772b50a3e8fd67638f40a3295fd565
Svetlin <svetlimladenov@gmail.com>
2020-04-22 18:52:57 +0300

    Fix Administration Area

--- src/SimpleSocial/Web/SimpleSocial.Web/Startup.cs
+++ src/SimpleSocial/Web/SimpleSocial.Web/Startup.cs
@@ -134,13 +134,14 @@ namespace SimpleSocial.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                
                 endpoints.MapRazorPages();
             });
         }
