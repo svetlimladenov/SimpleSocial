81d2d7e2a2cfa789cf19ce650e8bf0e6a4381092
Svetlin <svetlimladenov@gmail.com>
2020-05-30 01:09:31 +0300

    add web api donter core 3.1 project

---
+++ src/SimpleSocial/Web/SimpleSocial.WebAPI/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleSocial.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+    }
+}
