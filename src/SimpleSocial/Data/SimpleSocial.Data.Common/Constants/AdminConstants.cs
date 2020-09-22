70437ea1b582eed2831f9b51e2f51c9877c5582a
Svetlin <svetlimladenov@gmail.com>
2020-09-22 15:35:14 +0300

    Add Constants.

---
+++ src/SimpleSocial/Data/SimpleSocial.Data.Common/Constants/AdminConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSocial.Data.Common.Constants
+{
+    public static class AdminConstants
+    {
+        public const string AdminUsername = "admin";
+
+        public const string AdminPassword = "adminadmin";
+
+        public const string AdminEmail = "svetlimladenov@gmail.com";
+
+        public const string AdminRoleName = "Admin";
+    }
+}
