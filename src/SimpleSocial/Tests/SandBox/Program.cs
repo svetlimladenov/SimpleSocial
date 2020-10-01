7b6ae550c63d9978efaf9a5ab6f8e1eee72e2b67
Svetlin <svetlimladenov@gmail.com>
2020-10-01 14:00:10 +0300

    Clear out the code in the sandbox

--- src/SimpleSocial/Tests/SandBox/Program.cs
+++ src/SimpleSocial/Tests/SandBox/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +15,7 @@ namespace SandBox
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
@@ -24,53 +26,20 @@ namespace SandBox
             using (var serviceScope = serviceProvider.CreateScope())
             {
                 serviceProvider = serviceScope.ServiceProvider;
-                SandboxCode(serviceProvider);
+                await SandboxCode(serviceProvider);
             }
         }
 
-        private static void SandboxCode(IServiceProvider serviceProvider)
+        private static async Task SandboxCode(IServiceProvider serviceProvider)
         {
             var userRepo = serviceProvider.GetService<IRepository<SimpleSocialUser>>();
             var postRepo = serviceProvider.GetService<IRepository<Post>>();
 
-            var postContent = new List<string>()
-            {
-                "Happy New Year !!!",
-                "Merry Christmas !!!",
-                "Hi there",
-                "Good Morning",
-                "Good Morning, Friends",
-                "Good Afternoon",
-                "Good Evening",
-                "Night Out",
-                "I miss her",
-                "Do you miss me",
-                "I love music",
-                "I LOVE STACKOVERFLOW",
-                "I LOVE STACKOVERFLOW",
-                "I LOVE STACKOVERFLOW",
-                "I love dogs",
-                "Supp bitches",
-                "Yesterday was one of the best days in my entire life",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac diam vel justo consectetur ultrices. Nulla sed sollicitudin nunc, ac porta leo. Mauris dapibus quis est in porttitor. In vestibulum pretium eros sit amet porttitor. Phasellus quis fermentum libero. Curabitur egestas risus imperdiet molestie tincidunt. Suspendisse potenti. Donec suscipit imperdiet lacinia. Donec placerat dictum efficitur.",
-                "Vivamus non justo quis dolor iaculis vehicula nec sed diam. Nam placerat nibh eu luctus mattis. Vestibulum pulvinar purus sed scelerisque blandit. Nullam nec mauris ex. Vestibulum mauris lorem,"
-            };
-            var rnd = new Random();
-            foreach (var user in userRepo.All())
-            {
-                for (int i = 0; i < rnd.Next(10, 20); i++)
-                {
-                    var post = new Post
-                    {
-                        Content = postContent[rnd.Next(0, postContent.Count - 1)],
-                        UserId = user.Id,
-                        User = user
-                    };
-                    postRepo.AddAsync(post).GetAwaiter().GetResult();
-                    
-                }
-            }
-            postRepo.SaveChangesAsync().GetAwaiter().GetResult();
+            var dbContext = serviceProvider.GetService<SimpleSocialContext>();
+
+            var posts = await dbContext.Users.ToListAsync();
+
+            var a = 1;
         }
 
         private static void ConfigureServices(ServiceCollection services)
