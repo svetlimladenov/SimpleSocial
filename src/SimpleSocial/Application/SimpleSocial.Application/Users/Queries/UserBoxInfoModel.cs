4b3cb990b270f8ea50e3d161e0c3e688c9cebd7e
Svetlin <svetlimladenov@gmail.com>
2021-03-27 20:14:04 +0200

    Introduce new handler into the application layer

---
+++ src/SimpleSocial/Application/SimpleSocial.Application/Users/Queries/UserBoxInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SimpleSocial.Application.Users.Queries
+{
+    public class UserBoxInfoModel
+    {
+        public string Username { get; set; }
+
+        public string FullName { get; set; }
+
+        public int? Age { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public DateTime JoinedOn { get; set; }
+
+        public string Location { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+    }
+}
