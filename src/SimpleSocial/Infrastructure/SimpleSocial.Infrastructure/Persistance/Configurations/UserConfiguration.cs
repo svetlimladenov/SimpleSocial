e1dbc0a431e3efd607760278e7dc360bce45ad03
Svetlin <svetlimladenov@gmail.com>
2021-03-27 20:11:08 +0200

    Introduced new DbContext and Repository in the Clean Architecute

---
+++ src/SimpleSocial/Infrastructure/SimpleSocial.Infrastructure/Persistance/Configurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SimpleSocial.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSocial.Infrastructure.Persistance.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users", "identity");
+
+            builder.Property(x => x.Username).HasColumnName("UserName");
+        }
+    }
+}
---
+++ src/SimpleSocial/Infrastructure/SimpleSocial.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleSocial.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using SimpleSocial.Application.Repositories;
+using SimpleSocial.Infrastructure.Persistance.Repositories;
+
+namespace SimpleSocial.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection RegisterInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+               .AddDbContext<WebDbContext>(options => options
+                   .UseSqlServer(
+                       configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IUserReadonlyRepository, UserReadonlyRepository>();
+            
+            return services;
+        }
+    }
+}
