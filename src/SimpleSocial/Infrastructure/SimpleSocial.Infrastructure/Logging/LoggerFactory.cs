58ec0751c69cff974554e564d19647d59e8e8f44
Svetlin <svetlimladenov@gmail.com>
2021-04-24 16:43:21 +0300

    Add a logger factory

---
+++ src/SimpleSocial/Infrastructure/SimpleSocial.Infrastructure/Logging/LoggerFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace SimpleSocial.Infrastructure.Logging
+{
+    public static class LoggerFactory
+    {
+        public static Serilog.Core.Logger CreateLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .ConfigureFileSink(configuration)
+                .CreateLogger();
+        }
+
+        private static LoggerConfiguration ConfigureFileSink(this LoggerConfiguration loggerConfiguration, IConfiguration configuration)
+        {
+            const string section = "SimpleSocialLogging:Files";
+            const string enabled = "Enabled";
+            const string path = "Path";
+
+
+            var fileConfiguration = configuration.GetSection(section);
+            if (fileConfiguration.GetValue<bool>(enabled))
+            {
+                var fileDestination = fileConfiguration.GetValue<string>(path);
+                loggerConfiguration.WriteTo.File(fileDestination, rollingInterval: RollingInterval.Day);
+            }
+
+            return loggerConfiguration;
+        }
+    }
+}
