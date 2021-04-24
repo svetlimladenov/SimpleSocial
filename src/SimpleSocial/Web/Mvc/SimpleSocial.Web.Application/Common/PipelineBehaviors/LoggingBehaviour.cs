3652484f0513391e3bc442fa4f4486a2658a5e29
Svetlin <svetlimladenov@gmail.com>
2021-04-24 16:44:15 +0300

    Use the tracer in the mediator logging pipe

--- src/SimpleSocial/Web/Mvc/SimpleSocial.Web.Application/Common/PipelineBehaviors/LoggingBehaviour.cs
+++ src/SimpleSocial/Web/Mvc/SimpleSocial.Web.Application/Common/PipelineBehaviors/LoggingBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using SimpleSocial.Common.Logging;
+using SimpleSocial.Common.Tracing;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,22 +11,30 @@ namespace SimpleSocial.Web.Application.Common.PipelineBehaviors
     public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
         private readonly ISimpleSocialLogger<LoggingBehaviour<TRequest, TResponse>> logger;
+        private readonly ISimpleSocialTracer tracer;
 
-        public LoggingBehaviour(ISimpleSocialLogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        public LoggingBehaviour(ISimpleSocialLogger<LoggingBehaviour<TRequest, TResponse>> logger, ISimpleSocialTracer tracer)
         {
             this.logger = logger;
+            this.tracer = tracer;
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
+            var span = tracer.TryStartSpan(typeof(TRequest).Name, tracer.CurrentTransaction);
             try
             {
                 return await next();
             }
             catch (Exception exception)
             {
+                span?.CaptureException(exception);
                 LogException(exception);
                 throw;
+            }
+            finally
+            {
+                span?.End();
             }
         }
 
@@ -34,8 +43,8 @@ namespace SimpleSocial.Web.Application.Common.PipelineBehaviors
             this.logger.Error(
                 exception,
                 typeof(TRequest).Name,
-                "insert apm transaction here",
-                "insert apm transaction here");
+                traceId: tracer.CurrentTransaction.Id,
+                transactionId: tracer.CurrentTransaction.TraceId);
         }
     }
 }
