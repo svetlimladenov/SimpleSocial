e2ae55508b2c6756306fd99232ea915abc7d9e5c
Svetlin <svetlimladenov@gmail.com>
2021-10-30 23:57:46 +0300

    Add the defaults for the light theme

--- src/SimpleSocial/Web/react-ui/src/context/ThemeContext.js
+++ src/SimpleSocial/Web/react-ui/src/context/ThemeContext.js
@@ -6,6 +6,11 @@ export const themes = {
     secondary: "#24252a",
     text: "#fff",
   },
+  light: {
+    primary: "#f0f2f5",
+    secondary: "#fff",
+    text: "#65676b",
+  },
 };
 
 const ThemeContext = React.createContext(themes.dark);
